49f34f42d0e1f81da3b0a4dd574cff9b4398f5e3
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-07-01 19:54:41 +0530

    First Commit

---
+++ src/components/ResetButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./ResetButton.css";
+
+export const ResetButton = ({ resetBoard }) => {
+    return (
+        <button className="resetBtn" onClick={resetBoard}>Reset</button>
+    )
+}
